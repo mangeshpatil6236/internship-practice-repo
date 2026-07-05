84149e16c9f49eaae304472955f87ff383728b17
Mangesh Patil <mangesh07548p@gmail.com>
2026-07-05 17:13:46 +0530

    Initial Spring Boot Project

---
+++ src/main/java/com/demo/IntershipStudentManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipStudentManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershipStudentManagmentSystemApplication.class, args);
+	}
+
+}
